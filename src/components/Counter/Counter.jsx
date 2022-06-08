c3df8a21d35a9fcdcf0961028b195da474a721a9
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-06-08 21:02:51 +0530

    counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./counter.module.css";
 
-const Counter = () => {
+const Counter = (props) => {
   // sample value to be replaced
-  let count = 0;
+  let counter = 0;
+
+  const [count, setCount] = useState(props.value);
+  const handleChange = (val) => {
+    if (val === 0) {
+      setCount(0);
+    }  else {
+      if (count + val < 0) return;
+      setCount(count + val);
+    }
+  };
   // NOTE: do not delete `data-testid` key value pair
   return (
     <div className={styles.counter}>
-      <button data-testid="task-counter-increment-button">+</button>
-      <span data-testid="task-counter-value">{count}</span>
-      <button data-testid="task-counter-decrement-button">-</button>
+      <button data-testid="task-counter-increment-button"
+       onClick={() => {
+        handleChange(1);
+      }}
+      >+</button>
+      <span data-testid="task-counter-value">{counter}</span>
+      <button data-testid="task-counter-decrement-button"
+       onClick={() => {
+        handleChange(-1);
+      }}
+      >-</button>
     </div>
   );
 };
